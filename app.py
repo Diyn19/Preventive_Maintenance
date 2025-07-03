53545af88bede1c55368b3cce282f14e57eeb2f2
Diyn19 <hguc83356@gmail.com>
2025-07-03 13:04:33 +0800

    Update app.py

--- app.py
+++ app.py
@@ -156,3 +156,9 @@ def report():
         report_page=True,
         no_data_found=no_data_found
     )
+    
+import os
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
